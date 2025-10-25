078b3c83eaa1147330a004d829037d7dc0c5751d
nichkara <nina.reiss@nickr.eu>
2025-10-25 21:15:28 +0200

    Immediate type adjusted.

--- src/imm.vhd
+++ src/imm.vhd
@@ -6,9 +6,9 @@ use work.riscv_types.all;
 
 entity imm is
     port (
-        instr     : in  instruction;
-        opcode    : in  uOP;
-        immediate : out word
+        instr       : in  instruction;
+        format_type : in  imm_formats;
+        immediate   : out word
     );
 end entity imm;
 
@@ -17,36 +17,26 @@ architecture slicing of imm is
 
 begin
     -- Process immediate  slice
-    process (opcode, instr) is
+    process (format_type, instr) is
     begin
-        case opcode is
-                -- I-Type
-            when uLB | uLH | uLW | uLBU | uLHU | uADDI | uSLTI | uSLTIU | uXORI | uORI | uANDI =>
+        case format_type is
+            when I =>
                 immediate <= std_logic_vector(to_unsigned(0, wordWidth - 12)) & instr(31 downto 20);
-
-                -- S-Type
-            when uSB | uSH | uSW =>
+            when S =>
                 immediate <=
                     std_logic_vector(to_unsigned(0, wordWidth - 12)) & instr(31 downto 25) & instr(11 downto 7);
-
-                -- B-Type
-            when uBEQ | uBNE | uBLT | uBGE | uBLTU | uBGEU =>
+            when B =>
                 immediate <=
                     std_logic_vector(to_unsigned(0, 19)) & instr(31) & instr(7) & instr(30 downto 25) &
                         instr(11 downto 8) & "0";
-
-                -- U-Type
-            when uLUI | uAUIPC =>
+            when U =>
                 immediate <= instr(31 downto 12) & std_logic_vector(to_unsigned(0, 12));
-
-                -- J-Type
-            when uJAL =>
+            when J =>
                 immediate <=
                     std_logic_vector(to_unsigned(0, wordWidth - 21)) & instr(31) & instr(19 downto 12) & instr(20) &
                         instr(30 downto 21) & "0";
-
             when others =>
-                immediate <= x"C000FFEE";
+                immediate <= x"00000000";
         end case;
     end process;
 
